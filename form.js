809dd63ecf5b86a295fb1028c3f8b47bc92c853b
Brecht <brechtnulens@gmail.com>
2019-08-02 18:52:34 +0200

    added styling

--- form.js
+++ form.js
@@ -1,2 +1,16 @@
 const registration = document.getElementById("registration");
 
+let userInfo = {}
+let firstName = document.getElementById("first-name");
+let lastName = document.getElementById("last-name");
+let userName= document.getElementById("user-name");
+let phoneNumber = document.getElementById("phone-number");
+let personAge = document.getElementById("person-age")
+let personHobby = document.getElementById("hobbies")
+let emailInput = document.getElementById("email-input")
+let passwordInput = document.getElementById("password-input")
+let passwordConfirm = document.getElementById("confirm")
+let personCountry = document.getElementById("country")
+let consent = document.getElementById("consent")
+let signUp = document.getElementById("sign-up")
+let cancel = document.getElementById("cancel")
