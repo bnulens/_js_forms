4468b9303fb952da6eda618761aa4a1e19f7dc12
Brecht <brechtnulens@gmail.com>
2019-08-02 15:40:11 +0200

    empty

--- form.js
+++ form.js
@@ -2,17 +2,14 @@ const registration = document.getElementById("registration");
 
 // Challenge 2
 // Okay, now we have our HTML with a form. But nothing happens when we click on the submit input.
-
 // Find a way to store the data on submitting inside of variables/an object.
 
 // Challenge 3
 // Now that we have our data stored, we need to validate the user password. Do the passwords match?
-
 // Maybe its time to redirect the user, to a page that informs them about the successful registration?
 
 // Challenge 4
 // As you may guess, our next step is to validate if the user input matches our 'needs'. What I mean by this is, for example, A password must contain at least 1 number & 1 uppercase.
-
 // Our requirements are:
 
 // Email can only have '@gmail.com' other email providers are not allowed!
