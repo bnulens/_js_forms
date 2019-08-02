365dfa0c3b8ea626faae8855ef7b11620b8889ec
Brecht <brechtnulens@gmail.com>
2019-08-02 16:28:07 +0200

    start js

--- form.js
+++ form.js
@@ -1,29 +1,2 @@
 const registration = document.getElementById("registration");
 
-// Challenge 2
-// Okay, now we have our HTML with a form. But nothing happens when we click on the submit input.
-// Find a way to store the data on submitting inside of variables/an object.
-
-// Challenge 3
-// Now that we have our data stored, we need to validate the user password. Do the passwords match?
-// Maybe its time to redirect the user, to a page that informs them about the successful registration?
-
-// Challenge 4
-// As you may guess, our next step is to validate if the user input matches our 'needs'. What I mean by this is, for example, A password must contain at least 1 number & 1 uppercase.
-// Our requirements are:
-
-// Email can only have '@gmail.com' other email providers are not allowed!
-// Password must contain at least 1 number & 1 uppercase.
-// Bonus:
-// Take a look at regular expressions.
-
-// Challenge 5
-// If the user fills in the form incorrectly, there should appear feedback that tells the user what went wrong.
-
-// Requirements:
-
-// Input border should turn red (only for the ones that are failing).
-// An error message should tell the user what is required to succeed.
-// All input fields should stay filled in except for the incorrect ones.
-// Challenge 6
-// Now make the page beautiful with CSS, in any theme you want!
